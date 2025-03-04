26af11b9ee2ed5de61d378e069937498372405ef
Sipos Bogdan <bogdan.sipoa@gmail.com>
2025-03-04 05:26:23 +0200

    final draft

--- src/main/java/repository/pdf/PDFGenerateRepository.java
+++ src/main/java/repository/pdf/PDFGenerateRepository.java
@@ -1,9 +1,15 @@
 package repository.pdf;
 
+import model.Order;
+
+import java.io.FileNotFoundException;
+import java.util.List;
 import java.util.Map;
 
 public interface PDFGenerateRepository {
     Map<String, Map<String, Double>> fetchUserOrders();
     void generateUserOrdersReport(Map<String, Map<String, Double>> userEarnings);
     boolean generate();
+    List<Order> getAllOrders();
+    boolean generateOrdersReport();
 }
--- src/main/java/service/order/OrderServiceImpl.java
+++ src/main/java/service/order/OrderServiceImpl.java
@@ -3,6 +3,8 @@ package service.order;
 import model.Order;
 import repository.order.OrderRepository;
 
+import java.util.List;
+
 public class OrderServiceImpl implements OrderService {
     private OrderRepository orderRepository;
 
@@ -13,6 +15,11 @@ public class OrderServiceImpl implements OrderService {
     @Override
     public boolean save(Order order) {
         return orderRepository.create(order);
+    }
+
+    @Override
+    public List<Order> findAll() {
+        return orderRepository.findAll();
     }
 
 }
